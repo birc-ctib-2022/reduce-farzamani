a112434056d13e9c1e4003399cdee0ba63b0628f
Farhad Zamani <zmnfrhd@gmail.com>
2022-10-06 20:30:58 +0200

    initial commit

--- src/reduce.py
+++ src/reduce.py
@@ -11,10 +11,19 @@ def reduce(f: Callable[[A], B], x: list[A]) -> B:
 
     >>> reduce(lambda x,y: x+y, [1, 2, 3])
     6
+    >>> reduce(lambda x,y: x*y, [1, 2, 3, 4])
+    24
     """
     assert len(x) >= 2
-    ...  # FIXME
+    
+    # Initialize the first element
+    item = x[0]
 
+    # Iterate the operation until the last element
+    for i in range(1, len(x)):
+        item = f(item, x[i])
+    
+    return item
 
 def accumulate(f: Callable[[A], A], x: list[A]) -> list[A]:
     """
@@ -22,5 +31,21 @@ def accumulate(f: Callable[[A], A], x: list[A]) -> list[A]:
 
     >>> accumulate(lambda x,y: x+y, [1, 2, 3])
     [1, 3, 6]
+    >>> accumulate(lambda x,y: x*y, [1, 2, 3, 4])
+    [1, 2, 6, 24]
     """
-    ...  # FIXME
+    assert len(x) >= 2
+
+    # Create a list for the output
+    out_list = [0] * len(x)
+
+    # Initialize first element of the output list
+    out_list[0] = x[0]
+
+    # Iterate the operation
+    # from second element to the last element
+    # of the input list
+    for i in range(1, len(x)):
+        out_list[i] = f(out_list[i-1], x[i])
+
+    return out_list
